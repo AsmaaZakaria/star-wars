04696156812c087620a1d915db4377017460d925
AsmaaZakaria <asmaazakaria94@gmail.com>
2020-02-23 20:01:32 +0200

    clean code

--- src/api.js
+++ src/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-// this is a workaround so I avoided CORS block
 const URL = "https://swapi.co/api/";
-// const CORS_URL = `https://cors-anywhere.herokuapp.com/${URL}`;
 
 export default axios.create({
   baseURL: URL
