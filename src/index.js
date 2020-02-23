e39f3caf59142c9e2c0837191dccd3641c8383a2
AsmaaZakaria <asmaazakaria94@gmail.com>
2020-02-23 15:12:00 +0200

    router history

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { Router } from "react-router-dom";
-import { history } from "./history";
-import store from "./reduxUtils/store";
+import { createBrowserHistory } from "history";
 import App from "./components/App";
+import store from "./reduxUtils/store";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <Provider store={store}>
